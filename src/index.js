268825d28b016eabaa0a307c139eabf9ad9c6db9
Tan Wang <timmy940408@me.com>
2017-11-07 11:22:01 -0500

    refactor: Added thunk. Added a logger. Removed dev-tool

--- src/index.js
+++ src/index.js
@@ -7,11 +7,29 @@ import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from "redux";
 import reducer from './reducers';
+import ReduxThunk from 'redux-thunk'
 import {Provider} from 'react-redux';
 
-const store = createStore(reducer, 
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// const store = createStore(reducer, 
+//    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
+
+const store =  createStore(reducer, 
+    applyMiddleware(ReduxThunk, logger)
 )
+
+
+
+
 
 
 ReactDOM.render(
